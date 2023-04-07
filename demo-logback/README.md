a1904a89f4677679c99c127b08cad35d48d417b4
Ivan Wu <ivan.wu@elmosoftware.com.au>
2023-04-07 22:51:41 +1000

    spring boot logback

---
+++ demo-logback/README.md
@@ -0,0 +1,5 @@
+# spring-boot-demo-logback
+
+> This demo mainly demonstrates how to use logback to log the program's runtime process, and how to configure logback to generate both console logs and file logs. The file logs are split and generated based on date and size.
+
+The setting of logback is in the `logback-spring.xml` file in the `resources` directory.
---
+++ demo-logback/src/main/java/com/ivan/logback/SpringBootDemoLogbackApplication.java
@@ -0,0 +1,28 @@
+package com.ivan.logback;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+@Slf4j
+public class SpringBootDemoLogbackApplication {
+
+    public static void main(String[] args) {
+        ConfigurableApplicationContext context = SpringApplication.run(SpringBootDemoLogbackApplication.class, args);
+        int length = context.getBeanDefinitionNames().length;
+        log.info("SpringBootDemoLogbackApplication start success with {} Bean!", length);
+        log.error("SpringBootDemoLogbackApplication start success with {} Bean!", length);
+        log.debug("SpringBootDemoLogbackApplication start success with {} Bean!", length);
+        log.warn("SpringBootDemoLogbackApplication start success with {} Bean!", length);
+        log.trace("SpringBootDemoLogbackApplication start success with {} Bean!", length);
+
+        try {
+            int i = 0;
+            int j = 1 / i;
+        } catch (Exception e) {
+            log.error("【SpringBootDemoLogbackApplication】boot with error：", e);
+        }
+    }
+}
